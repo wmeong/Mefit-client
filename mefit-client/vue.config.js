8c7577ac6e1fcb1787b0bfcd8f54572d16a27512
khn <hanamango@naver.com>
2025-02-09 22:12:02 +0900

    feat: 운영계 버전으로 변경(소스맵 무시), 타이틀 변경

--- mefit-client/vue.config.js
+++ mefit-client/vue.config.js
@@ -4,16 +4,16 @@ module.exports = defineConfig({
     devServer: {
         proxy: {
             "/api": {
-                target: "https://api.mefit.co.kr", // Spring Boot 서버 URL
+                target: "https://api.mefit.co.kr",
                 changeOrigin: true,
-                pathRewrite: { "^/api": "/api" }, // 필요한 경우 경로 재작성
+                pathRewrite: { "^/api": "/api" },
             },
         },
     },
     // 소스 맵 활성화 설정
     configureWebpack: {
-        devtool: "source-map", // 소스 맵 생성 (로컬 크롬에서 디버깅을 하기 위함)
+        devtool: false, // 소스맵생성 : 개발계에서 true로 변경
     },
 
-    productionSourceMap: false,
+    productionSourceMap: false, //소스보기 : 개발계에서 true로 변경
 });
